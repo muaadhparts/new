17585c98e3ab074a88a3ab6ed0b951ac50f5e7f2
maheralmatari <maheralmatri@gmail.com>
2025-03-08 05:31:50 +0300

    finxhi note 1

--- resources/views/livewire/compatibility-tabs.blade.php
+++ resources/views/livewire/compatibility-tabs.blade.php
@@ -21,12 +21,43 @@
                     <h5>{{$catalog->name}}</h5>
                     <p>{{$catalog->data}}</p>
                     @dump($results)
-                    @if(filled($results))
-                    @foreach($results as $index => $result)
+{{--                    @if(filled($results))--}}
+                        <div class="container">
+                            <h4>Parts List</h4>
+                            <table class="table table-bordered">
+                                <thead>
+                                <tr>
+                                    <th>Part Number</th>
+                                    <th>Callout</th>
+                                    <th>Label  </th>
+                                    <th>Applicability</th>
+                                    <th>Code</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @forelse($results as $result)
+                                    <tr>
+                                        <td>{{ $result->partnumber }}</td>
+                                        <td>{{ $result->callout }}</td>
+                                        <td>{{ $result->label_en }}</td>
+                                        <td>{{ $result->applicability }}</td>
+                                        <td>{{ $result->code }}</td>
+                                    </tr>
+                                @empty
+                                    <tr>
+                                        <td colspan="5" class="text-center">No data found</td>
+                                    </tr>
+                                @endforelse
+                                </tbody>
+                            </table>
+                        </div>
 
-                            {{$result->code}}
-                    @endforeach
-                    @endif
+
+{{--                    @foreach($results as $index => $result)--}}
+
+{{--                            {{$result->code}}--}}
+{{--                    @endforeach--}}
+{{--                    @endif--}}
                 </div>
             @endif
         @endforeach
